b11ad3f26bda74341205b4962cfb4a73e3a08b62
Steph Samson <sdvsamson@gmail.com>
2014-01-14 21:56:23 +1100

    Add RSSFetch class

--- fetch.rb
+++ fetch.rb
@@ -3,23 +3,60 @@ require 'simple-rss'
 require 'open-uri'
 
 
-ARGV.map! { |http| "http://#{http}/"}
-ARGV.each do |website|
-	doc = Nokogiri::HTML(open(website))
-	check = doc.xpath("//link[@type=\"application/rss+xml\"]")
-	rss_path = check.map do |link|
-		if link['href'] =~ /^http:\/\//
-			link['href']
+class RSSFetch
+
+	attr_accessor :url
+	attr_reader :doc
+	
+	def initialize(url)
+		if url =~ %r{^http(s)?://}
+			@url = url			
+			@doc = Nokogiri::HTML(open(url))
 		else
-			"#{website}#{link['href']}"
+			raise "URL doesn't begin with HTTP!"
 		end
 	end
-	rss = SimpleRSS.parse open(rss_path[0])
-	rss.items.each do |item|
-		puts %Q{
-			#{item.title}
-			#{item.link}
-			Published on #{item.pubDate}
-		}
+
+	def has_feed?
+		!rss_links.empty?
+	end
+
+	def rss_links
+		doc.xpath("//link[@type=\"application/rss+xml\"]").map do |link|
+			if link['href'] =~ /^http:\/\//
+				link['href']
+			else
+				"#{@url}#{link['href']}"
+			end
+		end
+	end
+
+	def items
+		SimpleRSS.parse(open(rss_links.first)).items
+	end
+end
+
+
+ARGV.each do |arg|
+	begin
+		checker = RSSFetch.new(arg)
+		
+		if checker.has_feed?
+			puts "Items from #{arg}:"
+			checker.items.each do |item|
+				puts %Q{
+					#{item.title}
+					#{item.link}
+					Published on #{item.pubDate}
+				}
+			end
+			puts
+			puts
+		else
+			puts "#{arg} has no RSS feed."
+		end
+
+	rescue
+		puts "URL does not begin with HTTP."
 	end
 end
